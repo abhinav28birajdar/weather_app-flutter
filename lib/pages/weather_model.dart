1e89f50eb56c4760717868134a377c4bfcb103f5
YourNewUsername <youremail@example.com>
2024-11-17 09:11:19 +0530

    first chages

---
+++ lib/pages/weather_model.dart
@@ -0,0 +1,11 @@
+class weather{
+   final String cityName;
+   final double temperature;
+   final String maincondition;
+
+  weather({
+    required this.cityName,
+    required this.temperature,
+    require this.
+  });
+}
